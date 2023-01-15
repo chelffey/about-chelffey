313640bbf9d826a59fee57048276bc3a0536560d
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-15 22:27:25 +1100

    adding troll about me

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./pages.css";
+
+export default class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About me, [the website].</h1>
+        <p>
+          Thought you were learning more about Chelsea? Well you thought wrong -
+          cause you&apos;re actually talking to me, the website.
+        </p>
+        <p>I was born on a hot summers day. </p>
+        <h2>What am I made of</h2>
+        <p>Reactjs with Javascript, a full frontend hosted on GitHub pages.</p>
+        <p>Chelsea&apos;s frontend skills went UP after I was created.</p>
+        <p>Check out my repo - my blood and guts.</p>
+      </div>
+    );
+  }
+}
