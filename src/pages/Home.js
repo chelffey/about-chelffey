e6b5725ef2580396f26bd63583481780ddc87d2b
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-16 00:01:12 +1100

    updated design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,9 @@ export default class Home extends React.Component {
     return (
       <div>
         <div className="p-10">
-          <h2 className="text-3xl font-bold underline">Heya, its Chelsea.</h2>
-          <p>
-            Compsci student, software engineer, hobby artist and Japan
-            enthusiast.
+          <h2 className="text-3xl font-bold pb-5">Heya, its Chelsea.</h2>
+          <p className="italic">
+            Compsci student, software engineer and hobby artist.
           </p>
         </div>
         <div className="grid grid-cols-3 content-center p-5">
@@ -22,19 +21,20 @@ export default class Home extends React.Component {
               <li>Hopeful exchange student @UTokyo Japan</li>
             </ul>
           </div>
-          <img
-            className=""
-            src={require("../images/cat-logo.png")}
-            width="250"
-            alt="icon"
-          />
+          <section className="hero container max-w-screen-lg mx-auto pb-10">
+            <img
+              className="mx-auto w-36"
+              src={require("../images/cat-logo.png")}
+              alt="cat logo"
+            />
+          </section>
           <div>
             <ul>
               <li>INTP</li>
               <li>Proud owner of a (mostly functional) home-built PC</li>
               <li>Hobby artist and sketcher</li>
               <li>Avid genshin player and fanartist</li>
-              <li>Eucalyptus tree admirer and bubble tea enthusiast</li>
+              <li>Eucalyptus tree admirer</li>
               <li>Desperately wants a pet cat</li>
             </ul>
           </div>
@@ -43,9 +43,9 @@ export default class Home extends React.Component {
         <h2 className="text-2xl font-bold py-5">Technical Rundown</h2>
 
         <p>
-          I&apos;ve had experience in various fields, from backend to
-          datascience to frontend. I have a particular interest in frontend
-          engineering, desing and usability.
+          I&apos;ve had experience in various fields, from backend to data
+          science to frontend. I have a particular interest in frontend
+          engineering, design and usability.
         </p>
         <div className="pb-10">
           <h2 className="text-2xl font-bold py-5">Media</h2>
@@ -53,7 +53,7 @@ export default class Home extends React.Component {
             As part of Women In Technology Society (WIT), I&apos;ve had the
             chance to publish blog posts on topical topics.
           </p>
-          <p>I&apos;ve created various designs and covers for CSESoc.</p>
+          <p>I&apos;ve also created various designs and covers for CSESoc.</p>
 
           <h3 className="font-bold pt-3">Personal Work</h3>
           <p>
