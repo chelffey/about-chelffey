56e3eed7aa7306f1b9ac1ff7a8a15768862bd2ea
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-16 23:25:44 +1100

    colour scheme and centring process

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
-import Toolbar from "./components/Toolbar";
 import { Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <>
-      <Toolbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-me" element={<AboutMe />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,18 @@ import React from "react";
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="h-screen">
         <div className="p-10">
-          <h2 className="text-3xl font-bold pb-5">Heya, its Chelsea.</h2>
-          <p className="italic">
-            Compsci student, software engineer and hobby artist.
+          <h2 className="text-2xl italic pb-5">
+            part coder. part artist. part writer.
+          </h2>
+          <h2 className="text-3xl font-bold pb-5">full creator.</h2>
+          <p className="">
+            Meet Chelsea: Compsci student, software engineer and hobby artist.
           </p>
         </div>
-        <div className="grid grid-cols-3 content-center p-5">
-          <div>
+        <div className="grid grid-cols-3 content-center border-2 border-white">
+          <div className="border-2 border-white">
             <ul>
               {/* additional details on hover  */}
               <li>4th Year B. Computer Science @ UNSW</li>
@@ -19,18 +22,18 @@ export default class Home extends React.Component {
               <li>Casual Academic</li>
               <li>Multimedia VP and Creative Director</li>
               <li>Hopeful exchange student @UTokyo Japan</li>
+              <li>INTP</li>
             </ul>
           </div>
           <section className="hero container max-w-screen-lg mx-auto pb-10">
             <img
-              className="mx-auto w-36"
+              className="mx-auto"
               src={require("../images/cat-logo.png")}
               alt="cat logo"
             />
           </section>
           <div>
             <ul>
-              <li>INTP</li>
               <li>Proud owner of a (mostly functional) home-built PC</li>
               <li>Hobby artist and sketcher</li>
               <li>Avid genshin player and fanartist</li>
@@ -39,26 +42,12 @@ export default class Home extends React.Component {
             </ul>
           </div>
         </div>
-
-        <h2 className="text-2xl font-bold py-5">Technical Rundown</h2>
-
-        <p>
-          I&apos;ve had experience in various fields, from backend to data
-          science to frontend. I have a particular interest in frontend
-          engineering, design and usability.
-        </p>
-        <div className="pb-10">
-          <h2 className="text-2xl font-bold py-5">Media</h2>
-          <p>
-            As part of Women In Technology Society (WIT), I&apos;ve had the
-            chance to publish blog posts on topical topics.
-          </p>
-          <p>I&apos;ve also created various designs and covers for CSESoc.</p>
-
-          <h3 className="font-bold pt-3">Personal Work</h3>
-          <p>
-            Feel free to check out my art instagrams, @inkchaff and @charraxeon.
-          </p>
+        <div>
+          <img
+            className="block mx-auto w-1/5"
+            src={require("../images/cat-logo.png")}
+            alt="cat logo"
+          />
         </div>
       </div>
     );
