ba2b6abf11ddb27181bac2d7a59915cd147762c6
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-17 20:48:07 +1100

    updated image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,21 @@ export default class Home extends React.Component {
             part coder. part artist. part writer.
           </h2>
           <h2 className="text-5xl font-bold pb-5">full creator.</h2>
-          <p className="text-2xl">
-            Meet Chelsea: Compsci student, software engineer and hobby artist.
-          </p>
         </div>
         <div className="grid grid-cols-3 content-center px-20 pb-36 [&>*]:text-2xl">
           <div>
             <ul className="[&>*]:pb-6">
-              <li>4th Year B. Computer Science @UNSW</li>
+              <li>Age: 20</li>
+              <li>Pronouns: she/they</li>
+              <li>Major: Computer Science @UNSW</li>
+              <li>Year: Penultimate</li>
+              <li>INTP</li>
+            </ul>
+          </div>
+          <div></div>
+          <div>
+            <ul className="[&>*]:pb-6">
+              {/* additional details on hover  */}
               <li>Interning @Atlassian</li>
               <li>Casual Academic @UNSW</li>
               <li>Multimedia VP @UNSWWIT</li>
@@ -24,23 +31,11 @@ export default class Home extends React.Component {
               <li>exchange student @UTokyo</li>
             </ul>
           </div>
-          <div></div>
-          <div>
-            <ul className="[&>*]:pb-6">
-              {/* additional details on hover  */}
-              <li>INTP</li>
-              <li>Proud owner of a (mostly functional) home-built PC</li>
-              <li>Hobby artist and sketcher</li>
-              <li>Avid genshin player and fanartist</li>
-              <li>Eucalyptus tree admirer</li>
-              <li>Desperately wants a pet cat</li>
-            </ul>
-          </div>
         </div>
-        <div className="mx-auto bottom-0 fixed w-screen h-2/4">
+        <div className="mx-auto bottom-0 fixed w-screen h-2/3">
           <img
             className="mx-auto h-full"
-            src={require("../images/cat-logo.png")}
+            src={require("../images/cat-world-sleepy.png")}
             alt="cat logo"
           />
         </div>
