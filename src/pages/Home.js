0ca79267a4f6d0b512e7e7f27964e8b992ee3bd2
chelffey <chaffeychelsea@gmail.com>
2024-02-24 12:46:41 +1100

    basic text and sizes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,19 @@ import React from "react";
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="h-screen">
-        <h1 className="text-lg font-medium">
-          <br/>
-          Charley Chaffey<br/>
-          (they/them)
+      <div className="h-screen bg-gray-600 text-white font-sans h-screen flex flex-col items-center justify-center">
+        <h1 className="w-1/2 text-center">
+          <div className="text-2xl pt-5">
+            Hi, I&apos;m
+          </div>
+          <div className="text-8xl">
+            Charley
+          </div>
+          <div className="leading-loose pb-10">
+            (they/them)
+          </div>
         </h1>
-        <div>
+        <div className="text-2xl pb-10">
           I create things.
         </div>
 
@@ -21,7 +27,7 @@ export default class Home extends React.Component {
         
         <div>
           <h2>
-            from emotion to image
+            from emotion to art
           </h2>
         </div>
       </div>
