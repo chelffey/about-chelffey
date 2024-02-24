85a95ba890903db87c3c7787e10c6a1557d18cbf
chelffey <chaffeychelsea@gmail.com>
2024-02-24 12:29:50 +1100

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
-// import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about-me" element={<AboutMe />} /> */}
       </Routes>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,26 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="h-screen">
-        <br/>
-        Website under refresh - please check back later! :)
+        <h1 className="text-lg font-medium">
+          <br/>
+          Charley Chaffey<br/>
+          (they/them)
+        </h1>
+        <div>
+          I create things.
+        </div>
+
+        <div>
+          <h2>
+            from design to reality
+          </h2>
+        </div>
+        
+        <div>
+          <h2>
+            from emotion to image
+          </h2>
+        </div>
       </div>
     );
   }
