03310aa981e4517d62f1be426e67b417bc78f612
chelffey <chaffeychelsea@gmail.com>
2024-02-25 23:33:32 +1100

    text update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="bg-dark text-white font-sans pl-20 pr-20">
+      <div className="bg-dark text-white font-sans pl-20 pr-20 pb-32">
         <div className="h-screen flex flex-col items-center justify-center">
           <div className=" max-w-screen-sm w-full text-right ">
           ✦&nbsp; &nbsp; &nbsp;  ✩&nbsp;  ★&nbsp; &nbsp; &nbsp; ✶
@@ -21,40 +21,49 @@ export default class Home extends React.Component {
         Hi, I’m Charley (they/them). I fell into coding when I was in high-school, discovering the joy of of Python for the first time. Since then, I’ve been fascinated by the potential of my keyboard in building things - games, websites, models, and so much more. It has been matched only by my passion for art and design, and my drive to challenge myself. 
  
           </div>
+          <div> ↓ </div>
 
         </div>
-        <div>
+        <div className="flex flex-col items-center justify-center">
       
-          <div className="max-w-screen-sm pb-10">
-            <h2 className="text-4xl pb-5">
+          <div className="flex flex-col items-center justify-center h-screen">
+            <div className="max-w-screen-sm">
+              <h2 className="text-4xl pb-5">
             code
-            </h2>
-            <div>
+              </h2>
+              <div className="pb-10">
             During my studies in Computer Science (Co-op Program) @ UNSW, I’ve dipped into as many software disciplines as I could. I’ve interned in back-end engineering and product at WiseTech Global, R&D Machine Learning at ResMed, and finally, front-end engineering at Atlassian. JavaScript jokes aside, it was front-end that had me hooked - and like dark mode, I haven’t looked back since. 
-            </div>
-            <div>
+              </div>
+              <div>
             I love been able to see my code brings design to life and into the hands of users. 
+              </div>
+              <div className="text-center pt-10"> ↓ </div>
             </div>
           </div>
         
-          <div className="max-w-screen-sm pb-10">
-            <h2 className="text-4xl pb-5">
+          <div className="flex flex-col items-center justify-center h-screen">
+            <div className="max-w-screen-sm">
+              <h2 className="text-4xl pb-5">
             canvas
-            </h2>
-            <div>
-          My art is many things, but it is never static. It is a home where I can express my passions, from trees to movies, and my comfort spot where I can unwind, relax and re-energise myself. Though I constantly seek new challenges and to improve my skills as far as possible, I always remind myself that art-making for me is not a performance but a place of joy.
+              </h2>
+              <div>
+          My art is many things, but it is never static. It is a home where I can express my passions, from trees to movies, and my comfort spot where I can unwind, relax and re-energise myself. Though I constantly seek new challenges and to improve my skills as far as possible, I strive most to keep my artmaking as a place of joy.
+              </div>
+              <div className="text-center pt-10"> ↓ </div>
             </div>
           </div>
 
-          <div className="max-w-screen-sm pb-10">
-            <h2 className="text-4xl pb-5">
+          <div className="flex flex-col items-center justify-center h-screen">
+            <div className="max-w-screen-sm">
+              <h2 className="text-4xl pb-5">
           challenge
-            </h2>
-            <div>
+              </h2>
+              <div className="pb-10">
           In September 2023, I took a 9 hour flight to fulfil a childhood dream and tackle the ultimate programmer challenge - learning a human language. 
-            </div>
-            <div>
+              </div>
+              <div>
           Having deciding I’ve spent too much time ‘in the clouds’ (metaphorically and semi-literally), I went on an in-person exchange to study at Tokyo University as a Westpac Asian Exchange Scholar. With just a few months of prior Japanese language experience, I enrolled in five intensive language classes a week and then proceeded to learn basic survival skills for the next five months. It was an immensely eye-opening experience that has only strengthened my character and self-knowledge. After all, if I can survive half a year alone in a foreign country, what challenges can’t I overcome? 
+              </div>
             </div>
           </div>
         </div>
